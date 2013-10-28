065026fd6f819a9a0e6bfe27ca92a634af8414f9
tuananh <hvtuananh@gmail.com>
2013-10-28 13:26:27 -0400

    Display number of record found when querying for all lots

--- query.py
+++ query.py
@@ -219,6 +219,7 @@ except:
 This part is used to get all lots associated with a borough and block
 '''
 bbl_records = list(db.lot_records.find({'borough':borough,'block':block}, {'lot':1}))
+print "Found", len(bbl_records), "BBL in this area..."
 lots = set()
 for bbl_record in bbl_records:
     lots.add(bbl_record['lot'])
