49e11f84a847d846f5109870f8a3692c18899f52
tuananh <hvtuananh@gmail.com>
2013-11-03 20:03:21 -0500

    Change statistic script to consider both name and addresses

--- statistic.py
+++ statistic.py
@@ -12,19 +12,24 @@ db = client.furman
 
 primary = dict()
 secondary = dict()
-party_records = db.party_records.find({}, {'name_unified':1, 'party_type':1})
+party_records = db.party_records.find({}, {'name_unified':1, 'party_type':1, 'addr1_unified':1, 'addr2_unified':1, 'zip':1})
 for party_record in party_records:
+    try:
+        key = party_record['name_unified'] + party_record['addr1_unified'] + party_record['addr2_unified'] + str(party_record['zip'])
+    except:
+        continue
+        
     if party_record['party_type'] == 1:
         try:
-            secondary[party_record['name_unified']] += 1
+            secondary[key] += 1
         except:
-            secondary[party_record['name_unified']] = 1
+            secondary[key] = 1
     
     if party_record['party_type'] == 2:
         try:
-            primary[party_record['name_unified']] += 1
+            primary[key] += 1
         except:
-            primary[party_record['name_unified']] = 1
+            primary[key] = 1
             
 pickle.dump(primary, open('primary.txt', 'w'))
 pickle.dump(secondary, open('secondary.txt', 'w'))
