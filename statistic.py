a13fea5f526e6b892690d5df5200467d709272d6
tuananh <hvtuananh@gmail.com>
2013-11-04 15:35:42 -0500

    Change statistic script to look at new table

--- statistic.py
+++ statistic.py
@@ -7,25 +7,25 @@ This script will query for all statistic available in our database, regardless o
 from pymongo import MongoClient
 import pickle
 
-client = MongoClient('localhost', 40000)
+client = MongoClient('localhost', 40002)
 db = client.furman
 
 primary = dict()
 secondary = dict()
-party_records = db.party_records.find({}, {'name_unified':1, 'party_type':1, 'addr1_unified':1, 'addr2_unified':1, 'zip':1})
+party_records = db.party_records.find({}, {'NameUnified':1, 'Party_type':1, 'Addr1Unified':1, 'Addr2Unified':1, 'Zip':1})
 for party_record in party_records:
     try:
-        key = party_record['name_unified'] + party_record['addr1_unified'] + party_record['addr2_unified'] + str(party_record['zip'])
+        key = party_record['NameUnified'] + party_record['Addr1Unified'] + party_record['Addr2Unified'] + str(party_record['Zip'])
     except:
         continue
         
-    if party_record['party_type'] == 1:
+    if party_record['Party_type'] == 1:
         try:
             secondary[key] += 1
         except:
             secondary[key] = 1
     
-    if party_record['party_type'] == 2:
+    if party_record['Party_type'] == 2:
         try:
             primary[key] += 1
         except:
