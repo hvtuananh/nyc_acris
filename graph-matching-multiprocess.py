574779638714b68ff50b18e68d9eb792cd53825a
tuananh <hvtuananh@gmail.com>
2013-11-12 11:02:05 -0500

    Added script to calculate every pairs parallelly:

--- graph-matching-multiprocess.py
+++ graph-matching-multiprocess.py
@@ -4,12 +4,17 @@ from building import Building
 from bbl import BBL
 import pickle
 from multiprocessing import Process, Pool
+import traceback
 
 def calculate_similarities(bbl):
     print bbl
     results = list()
     for other in bbls:
-        results.append(calculate_similarity((bbl, other)))
+        try:
+            results.append(calculate_similarity((bbl, other)))
+        except:
+            traceback.print_exc()
+            print "This is an error!" + int(1)
     
 def calculate_similarity(pairs):
     return buildings[pairs[0]].similarity(buildings[pairs[1]])
@@ -19,7 +24,7 @@ buildings = pickle.load(open('buildings.bin'))
 #1. Compose a list of BBL:
 bbls = buildings.keys()
 
-pool = Pool(processes=50)
+pool = Pool(processes=60)
 results = pool.map(calculate_similarities, bbls)
 
 pickle.dump(results, open('match-scores.bin','w'))
